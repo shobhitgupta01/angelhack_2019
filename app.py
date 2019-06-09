761e7261b2b8186aaccb0d308bc737b03a5bb2ce
Shobhit Gupta <shobhit.gupta1995@gmail.com>
2019-06-09 06:05:11 +0530

    added charts

--- app.py
+++ app.py
@@ -1,22 +1,43 @@
+#importing libraries
 from flask import Flask, jsonify, render_template, url_for, request
 from get_text import get_text
 from flask_googlecharts import GoogleCharts, BarChart, MaterialLineChart
 from flask_googlecharts.utils import prep_data
 
+#initialization
 app = Flask(__name__)
+charts = GoogleCharts(app)
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
 
 @app.route('/topics',methods = ['POST','GET'])
 def topics():
     if request.method == 'POST':
         result = request.form
     url = result['url']
-    page_text = get_text(url)
-    print(page_text)
-    return render_template('results.html',result = page_text)
+    #page_text = get_text(url)
+    print(url)
+
+
+    #making google chart
+    my_chart = BarChart("my_chart", options={'title': 'My Chart'})
+
+    #data for chart
+    chart_data = [["technology",0.85],["maths",0.70],["war",0.6],['education',0.4],['candy',0.1]]
+
+    #adding data to chart
+    my_chart.add_column("Topic","Confidence Score")
+    my_chart.add_rows(chart_data)
+
+    #registering chart
+    charts.register(my_chart)
+
+    #rendering the page
+    return render_template('results.html',result = url)
 
 
 if __name__ == "__main__":
