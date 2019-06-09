02320477d49df83291448fb210854443e9a55a7d
Shobhit Gupta <shobhit.gupta1995@gmail.com>
2019-06-09 10:13:57 +0530

    sending post req to service

--- app.py
+++ app.py
@@ -24,8 +24,31 @@ def topics():
     #page_text = get_text(url)
     print(url)
 
+    
+
+    #fetching data from service
+    import requests
+
+    request_url = "http://10.2.89.90:5000/topics"
+
+    #payload = 
+    headers = {
+        'content-type': "application/json",
+        }
+
+    payload = str({'url':'{}'.format(url)})
+
+    response = requests.request("POST", request_url, data=payload, headers=headers)
+
+    response = response.text
+
+    strs = response.replace('[','').split('],')
+    lists = [map(int, s.replace(']','').split(',')) for s in strs]
+    
+    df = pd.DataFrame(lists,columns=['topic','score'])
+
     #data for chart
-    df = pd.DataFrame([['technology',0.85],['maths',0.70],['war',0.6],['education',0.4],['candy',0.1]],columns=["topic","score"])
+    #df = pd.DataFrame([['technology',0.85],['maths',0.70],['war',0.6],['education',0.4],['candy',0.1]],columns=["topic","score"])
    
     #rendering the page
     return render_template('results.html',df=df, titles=df.columns.values,team=team_name,url=url)
